73ec3446c9e6401c1f9406768b8d7b299442c4a5
Borivoje <cvetkovicborivoje@gmail.com>
2026-02-27 02:24:20 +0100

    fix: reduce logo icon and wordmark size to fit navbar

--- src/components/UnifiedNavbar.tsx
+++ src/components/UnifiedNavbar.tsx
@@ -56,7 +56,7 @@ export default function UnifiedNavbar({ variant, user: userProp, profileName: pr
                             alt="Workers United Logo"
                             width={80}
                             height={80}
-                            className="h-16 w-16 object-contain shrink-0"
+                            className="h-10 w-10 object-contain shrink-0"
                             priority
                         />
                         <Image
@@ -64,7 +64,7 @@ export default function UnifiedNavbar({ variant, user: userProp, profileName: pr
                             alt="Workers United"
                             width={200}
                             height={50}
-                            className="h-10 w-auto object-contain"
+                            className="w-[140px] h-auto object-contain"
                             priority
                         />
                     </Link>
