903ae0e50ff3c4a041ecdcef979d39c583a73a5d
Borivoje <cvetkovicborivoje@gmail.com>
2026-02-27 02:18:48 +0100

    fix: trim wordmark whitespace, adjust logo text size

--- src/components/UnifiedNavbar.tsx
+++ src/components/UnifiedNavbar.tsx
@@ -64,7 +64,7 @@ export default function UnifiedNavbar({ variant, user: userProp, profileName: pr
                             alt="Workers United"
                             width={200}
                             height={50}
-                            className="h-12 w-auto object-contain"
+                            className="h-10 w-auto object-contain"
                             priority
                         />
                     </Link>
