20c7145b563f90e1a23c89dfc369ae41c25efa99
BORIVOJE <borivoje@podovi.rs>
2026-03-04 17:09:51 +0100

    Fix mobile screen padding and reset max width limit to max-w-7xl

--- src/app/profile/worker/WorkerLayoutClient.tsx
+++ src/app/profile/worker/WorkerLayoutClient.tsx
@@ -29,7 +29,7 @@ export default function WorkerLayoutClient({
             <div className="flex-1 relative w-full flex">
                 <WorkerSidebar isOpen={isOpen} setIsOpen={setIsOpen} />
                 <div className={`flex-1 transition-all duration-300 min-w-0 pl-[84px] md:pl-0 ${isOpen ? 'md:ml-[280px]' : 'md:ml-[68px]'}`}>
-                    <main className="max-w-[1600px] mx-auto w-full px-4 sm:px-8 py-8">
+                    <main className="max-w-[1600px] mx-auto w-full px-4 sm:px-6 lg:px-8 py-6 lg:py-8">
                         {children}
                     </main>
                 </div>
